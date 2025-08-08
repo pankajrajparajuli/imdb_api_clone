639bdd31e0b2e2e7b9ca678e3bf73e8f581e6692
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-08 19:50:01 +0545

    fixed a syntax

--- imdb/watchlist_app/models.py
+++ imdb/watchlist_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Movies(models.Model):
+class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     release_date = models.DateField()
