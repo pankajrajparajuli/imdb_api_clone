db067edc1941f264aa8bdc5d9ba8d7ec6a66b2f1
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-09 15:29:45 +0545

    created watchlist function to list all movies data in json

--- imdb/watchlist_app/views.py
+++ imdb/watchlist_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . import models
+from django.http import JsonResponse
 
 # Create your views here.
+
+def watchlist(request):
+    movies = models.Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+    }
+    return JsonResponse(data)
+
