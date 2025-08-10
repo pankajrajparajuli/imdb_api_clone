a7d463b9a763042eb51df354fb425bf8dae7ec41
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-10 16:04:30 +0545

    undid removed

--- imdb/watchlist_app/api/serializers.py
+++ imdb/watchlist_app/api/serializers.py
@@ -81,3 +81,27 @@ class WatchListSerializer(serializers.ModelSerializer):
         if not isinstance(value, bool):
             raise serializers.ValidationError("Active must be a boolean value.")
         return value
+
+""" class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    active = serializers.BooleanField()
+    release_date = serializers.DateField()
+    rating = serializers.DecimalField(max_digits=3, decimal_places=1)
+    created_at = serializers.DateTimeField()
+    
+    def create(self, validated_data):
+        Movie.objects.create(**validated_data)
+        return validated_data
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.rating = validated_data.get('rating', instance.rating)
+        instance.save()
+        return instance """
+    
+    
