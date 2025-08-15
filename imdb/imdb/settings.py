8ecf09d3758fe4e367c815abc5fc0740146cc059
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-15 23:54:28 +0545

    custom time of access token and refresh token

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -142,3 +142,10 @@ REST_FRAMEWORK = {
         #'rest_framework.authentication.BasicAuthentication',
     ],
 }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    }
