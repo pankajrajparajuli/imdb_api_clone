75ce656f4f03ec69b2b5736781286e9859352a8c
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-15 23:30:43 +0545

    added JWT Token auth

--- imdb/account_app/api/urls.py
+++ imdb/account_app/api/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 urlpatterns = [
     path('login/', obtain_auth_token, name='api-login'),
     path('register/', views.register, name='api-register'),
     path('logout/', views.logout, name='api-logout'),
+    
+    
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -137,7 +137,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ], """
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.JWTAuthentication',
+        #'rest_framework.authentication.TokenAuthentication',
         #'rest_framework.authentication.BasicAuthentication',
     ],
 }
