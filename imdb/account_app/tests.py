c74868d4fda91cf9ef9227f01e97546e3cb6d9b2
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-16 15:04:17 +0545

    wrote test for user reg

--- imdb/account_app/tests.py
+++ imdb/account_app/tests.py
@@ -1,8 +1,22 @@
-from django.test import TestCase
-
+from urllib import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 # Create your tests here.
 
-
+class UserRegistrationTests(APITestCase):
+    def test_user_registration(self):
+        data = {
+            'username': 'testuser',
+            'email': 'testcase@example.com',
+            'password': 'testpassword',
+            'password2': 'testpassword'
+            }
+        self.client.post(reverse('api-register'), data, format='json')
+        self.assertEqual(response.status_code , status.HTTP_201_CREATED)
+        
 
 
 
